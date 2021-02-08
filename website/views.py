43425e36dd8d630a10370e806f7f6c3695997591
Teejay <tstech3011@gmail.com>
2021-02-08 11:30:58 +0530

    First Commit

--- website/views.py
+++ website/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
 def index(request):
-    context =   {}
-    return render(request, 'website/index.html', context)
+    if request.method == "POST":
+        username = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        return render(request, 'website/index.html', {'name' : username})
+
+    else:   
+        return render(request, 'website/index.html')
+
+
